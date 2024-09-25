e5cf8ef59c2676455baa68bef82b98422a67a698
Aayan <aayank1306@gmail.com>
2024-09-25 13:58:59 +0530

    vercel analytics setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import Header from "@/components/Header";
+import { Analytics } from "@vercel/analytics/react"
 
 export const metadata: Metadata = {
   title: "Aayan Khan",
@@ -19,6 +20,7 @@ export default function RootLayout({
         {children}
         <div className="background-gradient absolute inset-0 -z-50 max-h-screen" />
         {/* <div className="pointer-events-none absolute inset-0 -z-40 h-full bg-[url('/noisetexture.jpg')] opacity-20 mix-blend-soft-light"></div> */}
+        <Analytics />
       </body>
     </html>
   );
